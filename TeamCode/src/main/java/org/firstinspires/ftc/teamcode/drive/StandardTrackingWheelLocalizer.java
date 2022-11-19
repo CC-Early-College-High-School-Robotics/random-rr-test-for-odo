46cda333c632752a95aac65813e4e7e8a8a618c4
Anti-Shulk <84753002+Anti-Shulk@users.noreply.github.com>
2022-11-19 14:51:23 -0600

    tuning odo pods and fixing odo pod and mech drive directions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,11 +31,11 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.6889764; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 11.3386;  // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 11.17; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = -5.511815; // in; offset of the lateral wheel
 
-    public static double X_MULTIPLIER = 1; // Multiplier in the X direction
-    public static double Y_MULTIPLIER = 1; // Multiplier in the Y direction
+    public static double X_MULTIPLIER = 1.028; // Multiplier in the X direction
+    public static double Y_MULTIPLIER = 1.065; // Multiplier in the Y direction
 
     private Encoder leftEncoder, rightEncoder, rearEncoder;
 
